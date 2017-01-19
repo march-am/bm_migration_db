c515f0739920fa16c6a45da93abfb4a1ed586722
march-am <myredblood@gmail.com>
2017-01-19 12:58:49 +0900

    add files

---
+++ imports.sql
@@ -0,0 +1,15 @@
+CREATE TABLE book_ids (
+  bookID text
+);
+
+CREATE TABLE books (
+  bookID text,
+  title text,
+  author text,
+  rank integer,
+  status text,
+  review text,
+  tag text,
+  memo text,
+  read_at
+);
